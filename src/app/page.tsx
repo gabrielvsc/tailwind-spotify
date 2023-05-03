23616f7c6011845ef567a03cf091143202136a76
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2023-05-03 18:42:36 -0300

    feat: first section/albums main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from 'lucide-react';
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -43,6 +44,59 @@ export default function Home() {
             <button className="rounded-full bg-black/40 p-1">
               <ChevronRight />
             </button>
+          </div>
+
+          <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
+        
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a href="" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/eve-bokurano.jpg" width={104} height={104} alt=""/>
+              <strong>Bokurano</strong>
+              
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/eve-kakaikitan.jpg" width={104} height={104} alt=""/>
+              <strong>Bokurano</strong>
+
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/eve-smile.jpg" width={104} height={104} alt=""/>
+              <strong>Bokurano</strong>
+
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/eve-bokurano.jpg" width={104} height={104} alt=""/>
+              <strong>Bokurano</strong>
+
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/eve-kakaikitan.jpg" width={104} height={104} alt=""/>
+              <strong>Bokurano</strong>
+
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a href="" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/eve-smile.jpg" width={104} height={104} alt=""/>
+              <strong>Bokurano</strong>
+
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
           </div>
         </main>
       </div>
