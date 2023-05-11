c80d149f14fe2228b120176c763a8c5d5690a925
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2023-05-10 22:32:46 -0300

    feat: add music progress bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -151,7 +151,13 @@ export default function Home() {
             <SkipForward size={20} className="text-zinc-200"/>
             <Repeat size={20} className="text-zinc-200"/>
           </div>
-          
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">0:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
+            <span className="text-xs text-zinc-400">3:31</span>
+          </div>
         </div>
 
         <div></div>
